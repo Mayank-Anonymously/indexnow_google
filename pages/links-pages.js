2837749e45d4eae2612642a6b4d53c3509065025
Mayank Kumar <developer-at-work@Mayanks-MacBook-Air.local>
2025-11-15 11:47:59 +0530

    added changes

--- pages/links-pages.js
+++ pages/links-pages.js
@@ -3,8 +3,17 @@ import React from 'react';
 const linksAvailable = () => {
 	return (
 		<>
-			<a href='https://msdprojectclear.org/_resources/_MF/data/form_16480/files/element_6_4472943eaf839a462a6944e07df4d235-Cheap%20Expedia%20Flights.pdf.tmp'>
-				https://msdprojectclear.org/_resources/_MF/data/form_16480/files/element_6_4472943eaf839a462a6944e07df4d235-Cheap%20Expedia%20Flights.pdf.tmp
+			<a href='https://intelligencestudies.utexas.edu/wp-content/uploads/ninja-forms/3/quick-help-how-much-does-it-cost-to-upgrade-a-flight-to-business.pdf'>
+				https://intelligencestudies.utexas.edu/wp-content/uploads/ninja-forms/3/quick-help-how-much-does-it-cost-to-upgrade-a-flight-to-business.pdf
+			</a>
+			<a href='https://intelligencestudies.utexas.edu/wp-content/uploads/ninja-forms/3/delta-support-how-to-upgrade-for-free-with-delta.pdf'>
+				https://intelligencestudies.utexas.edu/wp-content/uploads/ninja-forms/3/delta-support-how-to-upgrade-for-free-with-delta.pdf
+			</a>
+			<a href='https://intelligencestudies.utexas.edu/wp-content/uploads/ninja-forms/3/glidefree-how-much-does-it-cost-to-upgrade-to-delta-first-class.pdf'>
+				https://intelligencestudies.utexas.edu/wp-content/uploads/ninja-forms/3/glidefree-how-much-does-it-cost-to-upgrade-to-delta-first-class.pdf
+			</a>
+			<a href='https://intelligencestudies.utexas.edu/wp-content/uploads/ninja-forms/3/can-i-pay-for-an-upgrade-on-deltariseabove.pdf'>
+				https://intelligencestudies.utexas.edu/wp-content/uploads/ninja-forms/3/can-i-pay-for-an-upgrade-on-deltariseabove.pdf
 			</a>
 		</>
 	);
